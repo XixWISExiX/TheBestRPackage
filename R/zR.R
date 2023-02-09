a9e93e32d498bc781414f3ad84c4a6fd46f20a4b
XixWISExiX <addalooptoit@gmail.com>
2023-02-08 21:02:22 -0600

    Upload of the Best R Package

---
+++ R/zR.R
@@ -0,0 +1,16 @@
+#'  Make a vector of z values
+#'
+#' @param x A vector of numeric values.
+#'
+#'
+#' @return A vector of z values.
+#' @export
+#' @import stats
+#'
+#' @examples
+#' myz(1:4)
+
+myz <- function(x){
+  z <- (x-mean(x))/sd(x)
+  z
+}
